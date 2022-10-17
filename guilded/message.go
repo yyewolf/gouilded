9dd8563adac5e6764cc64852d29416db0a87ad75
Tristan SMAGGHE <tristan.smagghe@telecomnancy.net>
2022-10-17 08:27:57 +0200

    [+] Messages

--- guilded/message.go
+++ guilded/message.go
@@ -1,6 +1,7 @@
 package guilded
 
 import (
+	"encoding/json"
 	"time"
 )
 
@@ -15,36 +16,74 @@ type Message struct {
 	// ID is the ID of the message
 	ID string `json:"id"`
 
+	// Type is the type of the message
+	Type MessageType `json:"type"`
+
+	// ServerID is the ID of the server the message was sent in
+	ServerID string `json:"serverId"`
+
 	// ChannelID is the ID of the channel the message was sent in
 	ChannelID string `json:"channelId"`
 
-	// Author is the User who created the message
-	// Author *User `json:"createdBy"`
-
 	// Content is the content of the message
-	// Content *MessageContent `json:"content"`
+	Content string `json:"content"`
+
+	// Embeds is the list of embeds in the message
+	Embeds []*MessageEmbed `json:"embeds"`
+
+	// ReplyMessageIDs is the list of message IDs this message is replying to
+	ReplyMessageIDs []string `json:"replyMessageIds"`
+
+	// IsPrivate, if true, indicates that the message is only seen by those mentioned
+	IsPrivate bool `json:"isPrivate"`
+
+	// IsSilent, if true, indicates that the message did not ping
+	IsSilent bool `json:"isSilent"`
+
+	// Mentions is the list of users mentioned in the message
+	Mentions []*Mention `json:"mentions"`
 
 	// CreatedAt is the time the message was created at in ISO 8601 format
 	CreatedAt time.Time `json:"createdAt"`
 
-	// EditedAt is the time the message was edited at in ISO 8601 format
-	EditedAt time.Time `json:"editedAt"`
+	// Author is the user who sent the message
+	// If the message was sent by a webhook, this will be set to 'Ann6LewA'
+	Author *User `json:"-"`
 
-	// DeletedAt is the time the message was deleted at in ISO 8601 format
-	DeletedAt time.Time `json:"deletedAt"`
+	// CreatedByWebhookID is the ID of the webhook that created the message
+	CreatedByWebhookID string `json:"createdByWebhookId"`
 
-	// Reactions is a list of reactions on the message
-	// Reactions []*Reaction `json:"reactions"`
+	// UpdatedAt is the time the message was last updated at in ISO 8601 format
+	UpdatedAt time.Time `json:"updatedAt"`
+}
 
-	// IsPinned is true if the message is pinned
-	IsPinned bool `json:"isPinned"`
+func (m *Message) UnmarshalJSON(b []byte) error {
+	type Alias Message
+	aux := &struct {
+		Author string `json:"createdBy"`
+		*Alias
+	}{
+		Alias: (*Alias)(m),
+	}
 
-	// PinnedBy is the User who pinned the message
-	// PinnedBy *User `json:"pinnedBy"`
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
 
-	// WebhookID is the ID of the webhook that sent the message
-	WebhookID string `json:"webhookId"`
+	m.Author = &User{UserID: aux.Author}
 
-	// Type is the type of the message
-	Type MessageType `json:"type"`
+	return nil
+}
+
+func (m *Message) MarshalJSON() ([]byte, error) {
+	type Alias Message
+	aux := &struct {
+		Author string `json:"createdBy"`
+		*Alias
+	}{
+		Alias:  (*Alias)(m),
+		Author: m.Author.UserID,
+	}
+
+	return json.Marshal(aux)
 }
